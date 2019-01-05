c18d5c5519bb9cdc3b7160cbb2d2345484b2afd9
Joseph MC <43304899+JMCHunter@users.noreply.github.com>
2019-01-05 03:46:33 -0500

    Amanecidota Vol. IX - Sustentacion

---
+++ src/app/Models/unidad.ts
@@ -0,0 +1,6 @@
+export class Unidad {
+    email: string;
+    latitud: number;
+    longitud: number;
+    uid: string;
+}
--- src/app/dashboard-real-time/dashboard-real-time.component.ts
+++ src/app/dashboard-real-time/dashboard-real-time.component.ts
@@ -1,5 +1,8 @@
-import { MapsComponent } from './../maps/maps.component';
 import { Component, OnInit } from '@angular/core';
+import { Unidad } from '../Models/unidad';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 declare const google: any;
 
@@ -10,10 +13,19 @@ declare const google: any;
 })
 export class DashboardRealTimeComponent implements OnInit {
 
-  constructor() { }
+  public units: Observable<any[]>;
+  public currentUnit: Unidad;
 
-  ngOnInit() {
-    const myLatlng = new google.maps.LatLng(-12.1397046, -76.9996628);
+  constructor(private db: AngularFireDatabase) {
+    this.units = this.db.list('Unidades').snapshotChanges().pipe(
+        map(changes => changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+        )
+      );
+  }
+
+  onShow(unit: Unidad) {
+    this.currentUnit = unit;
+    const myLatlng = new google.maps.LatLng(unit.latitud, unit.longitud);
     const mapOptions = {
         zoom: 13,
         center: myLatlng,
@@ -105,7 +117,7 @@ export class DashboardRealTimeComponent implements OnInit {
         }]
 
     };
-    const map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    const mapa = new google.maps.Map(document.getElementById('map'), mapOptions);
 
     const marker = new google.maps.Marker({
         position: myLatlng,
@@ -113,6 +125,110 @@ export class DashboardRealTimeComponent implements OnInit {
     });
 
     // To add the marker to the map, call setMap();
-    marker.setMap(map);
+    marker.setMap(mapa);
+  }
+
+  ngOnInit() {
+    const myLatlng = new google.maps.LatLng(-12.1633, -76.9636);
+    const mapOptions = {
+        zoom: 13,
+        center: myLatlng,
+        scrollwheel: false, // we disable de scroll over the map, it is a really annoing when you scroll through page
+        styles: [{
+            'featureType': 'water',
+            'stylers': [{
+                'saturation': 43
+            }, {
+                'lightness': -11
+            }, {
+                'hue': '#0088ff'
+            }]
+        }, {
+            'featureType': 'road',
+            'elementType': 'geometry.fill',
+            'stylers': [{
+                'hue': '#ff0000'
+            }, {
+                'saturation': -100
+            }, {
+                'lightness': 99
+            }]
+        }, {
+            'featureType': 'road',
+            'elementType': 'geometry.stroke',
+            'stylers': [{
+                'color': '#808080'
+            }, {
+                'lightness': 54
+            }]
+        }, {
+            'featureType': 'landscape.man_made',
+            'elementType': 'geometry.fill',
+            'stylers': [{
+                'color': '#ece2d9'
+            }]
+        }, {
+            'featureType': 'poi.park',
+            'elementType': 'geometry.fill',
+            'stylers': [{
+                'color': '#ccdca1'
+            }]
+        }, {
+            'featureType': 'road',
+            'elementType': 'labels.text.fill',
+            'stylers': [{
+                'color': '#767676'
+            }]
+        }, {
+            'featureType': 'road',
+            'elementType': 'labels.text.stroke',
+            'stylers': [{
+                'color': '#ffffff'
+            }]
+        }, {
+            'featureType': 'poi',
+            'stylers': [{
+                'visibility': 'off'
+            }]
+        }, {
+            'featureType': 'landscape.natural',
+            'elementType': 'geometry.fill',
+            'stylers': [{
+                'visibility': 'on'
+            }, {
+                'color': '#b8cb93'
+            }]
+        }, {
+            'featureType': 'poi.park',
+            'stylers': [{
+                'visibility': 'on'
+            }]
+        }, {
+            'featureType': 'poi.sports_complex',
+            'stylers': [{
+                'visibility': 'on'
+            }]
+        }, {
+            'featureType': 'poi.medical',
+            'stylers': [{
+                'visibility': 'on'
+            }]
+        }, {
+            'featureType': 'poi.business',
+            'stylers': [{
+                'visibility': 'simplified'
+            }]
+        }]
+
+    };
+    const mapa = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    const marker = new google.maps.Marker({
+        position: myLatlng,
+        title: 'Hello World!'
+    });
+
+    // To add the marker to the map, call setMap();
+    marker.setMap(mapa);
   }
 }
